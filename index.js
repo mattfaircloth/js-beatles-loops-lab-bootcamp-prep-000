caed7703bbfd1499102c9076b36ffd30d8f8dec2
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-04 14:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay (musicians, instruments) {
   var finalArray = [];
 
   for (var i = 0; i < musicians.length; i++) {
-    finalArray.push(musicians[i] + ' plays' + instruments[i])
+    finalArray.push(musicians[i] + ' plays ' + instruments[i])
   }
   return finalArray;
 }
